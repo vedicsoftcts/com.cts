399f4b7c3f52668fa087e0814b45ca168b9b431c
Latif Vedic <latif@vedic.com>
2021-06-07 10:52:50 +0530

    1st maven project

---
+++ src/main/java/com/cts/App.java
@@ -0,0 +1,18 @@
+package com.cts;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a=20;
+        int b=10;
+        if (a > b) {
+        	System.out.println("a is greate than b");
+        }
+        else {
+        		System.out.println(" a is not greater than b");
+        }
+                
+    }
+}
